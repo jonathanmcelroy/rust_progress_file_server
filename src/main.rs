4cdf937f82baa970b6a4ab588f9aa0644838243d
Jonathan McErloy <jmcelroy@datasystemstx.com>
2016-08-11 16:34:41 -0500

    Steps

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,28 @@ extern crate hyper;
 extern crate rustc_serialize;
 extern crate walkdir;
 extern crate regex;
+extern crate ini;
 
 use nickel::{Nickel, HttpRouter, Mountable};
 use std::path::Path;
 use rustc_serialize::json;
 use walkdir::WalkDir;
 use regex::Regex;
+use ini::Ini;
 
 const PATH: &'static str = "C:/stec82";
+
+fn get_propath() -> Result<Vec<String>, Error> {
+    let mut stec_ini = try!(get_file(concat!(PATH, "/stec.ini")));
+    stec_ini = stec_ini.replace("\\", "/");
+    
+    let conf = try!(Ini::load_from_str(&stec_ini).map_err(|err| Error::Ini("Could not parse ini file", err)));
+
+    conf.section(Some("Startup"))
+        .and_then(|section| section.get("PROPATH"))
+        .map(|s| s.split(",").map(|s| String::from(s)).collect())
+        .ok_or(Error::General("No PROPATH field"))
+}
 
 fn main() {
     let mut server = Nickel::new();
@@ -40,12 +54,11 @@ fn main() {
 
     let find_regex = Regex::new("/find/(?P<contents>.+)").unwrap();
     server.get(find_regex, middleware! { |req, res| {
-        let contents = req.param("contents").unwrap();
-        println!("contents: {}", contents);
+        let contents : &str = &req.param("contents").unwrap().replace("%2F", "/");
         let mut results = vec!();
         for entry in WalkDir::new(PATH).into_iter().filter_map(|e| e.ok()) {
             // println!("File: {:?}, {:?}", entry, entry.file_name());
-            if entry.file_type().is_file() && entry.file_name() == contents {
+            if entry.file_type().is_file() && (entry.file_name() == contents) {
                 results.push(String::from(entry.path().to_str().unwrap()));
             }
         }
